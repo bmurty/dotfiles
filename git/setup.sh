f4cb03e1fe19f2b948a95acea17126d34248d3ba
Brendan Murty <brendan@murty.au>
2026-01-24 10:28:34 +1100

    Cleanup old scripts

--- git/setup.sh
+++ git/setup.sh
@@ -6,27 +6,3 @@ cp ~/.gitignore ~/.gitignore.before-dotfiles-base.bak
 
 cat custom.gitconfig >> ~/.gitconfig
 cat custom.gitignore >> ~/.gitignore
-
-# Clone repositories from `repositories.txt` to folders under `GIT_REPOSITORY_FOLDER`
-#  - Originally found here: https://github.com/rkalis/dotfiles/blob/master/repos/setup.sh
-GIT_REPOSITORY_FOLDER="$HOME/Repositories"
-GIT_REPOSITORY_LIST="$(pwd)/repositories.txt"
-if [[ -f "repositories.txt" ]]; then
-  echo "INFO  Loading repositories from '$GIT_REPOSITORY_LIST' in to directories under '$GIT_REPOSITORY_FOLDER'"
-
-  mkdir -p $GIT_REPOSITORY_FOLDER
-  cd $GIT_REPOSITORY_FOLDER
-
-  while read repo; do
-    if [[ $repo == \#* ]]; then continue; else # Skip comment lines in the file
-      if [[ $repo != "" ]]; then # Skip empty lines in the file
-        echo "INFO  Running 'git clone $repo'..."
-        git clone $repo &> /dev/null
-      fi
-    fi
-  done < $GIT_REPOSITORY_LIST
-
-  echo "DONE  Finished cloning Git repositories"
-else
-  echo "INFO  No repository md file found, this can be recreated by copying 'repositories.example.txt'"
-fi
